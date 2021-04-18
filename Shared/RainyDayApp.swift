89ceaec2389ddbe69fe25d4e074a03dc7db7c2bd
Chan Wai Hsuen <chanwaihsuen@gmail.com>
2021-04-18 20:17:06 +0800

    Initial Commit

---
+++ Shared/RainyDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainyDayApp.swift
+//  Shared
+//
+//  Created by Chan Wai Hsuen on 18/4/21.
+//
+
+import SwiftUI
+
+@main
+struct RainyDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
