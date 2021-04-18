e17ef47c7680624dc92a671914ec551a0106cfc5
Chan Wai Hsuen <chanwaihsuen@gmail.com>
2021-04-18 20:52:29 +0800

    pod added

--- Shared/RainyDayApp.swift
+++ Shared/RainyDayApp.swift
@@ -6,12 +6,22 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct RainyDayApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+
+
+
